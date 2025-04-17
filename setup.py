5b37db82ad1b76fa02498ed101a638401bbbfede
Radim Mifka <radimmifka@gmail.com>
2025-04-17 15:22:56 +0200

    fix: nix build and shell

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ setup(
     packages=[
         "cache_server_app",
         "cache_server_app/src",
+        "cache_server_app/src/cache",
+        "cache_server_app/src/commands",
+        "cache_server_app/src/config",
+        "cache_server_app/src/parser",
         "cache_server_app/src/storage",
+        "cache_server_app/src/storage/providers",
     ],
-    install_requires=["websockets", "pyjwt", "ed25519"],
+    install_requires=["websockets", "pyjwt", "ed25519", 'boto3', 'pyyaml'],
     entry_points={
         "console_scripts": ["cache-server = cache_server_app.main:main"],
     },
